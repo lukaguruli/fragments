2717ffcff49433c461597c8356fdb77055dfde83
lukaguruli <luka.guruli.1@btu.edu.ge>
2020-12-09 22:56:29 +0400

    first commit

---
+++ app/src/main/java/com/example/myapplicationfragment/fragments/DashboardFragment.kt
@@ -0,0 +1,7 @@
+package com.example.myapplicationfragment.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.myapplicationfragment.R
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+}
